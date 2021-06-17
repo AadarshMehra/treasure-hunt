a0d6bcbd90c37622f6cf41b2c231f9c18c51804d
AadarshMehra <61242909+AadarshMehra@users.noreply.github.com>
2021-06-17 11:03:32 +0530

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  invite: 'NTBlMGQzMDAtY2E4ZS0xMWViLWJkNzEtZjFmMjgyZGY3NjVkOmYwYTE0NjdhLTMyYzEtMTFlYi1iMWI4LTViYTQzMmY1ZjBkMA==',
+  invite: 'MmQwNmU3ZWUtY2YyYy0xMWViLWI4ZjItY2QzYjc2ODI0NzEzOmYwYTE0NjdhLTMyYzEtMTFlYi1iMWI4LTViYTQzMmY1ZjBkMA==',
   password: 'SvnKg7zFwkLc9mh9PAAWt',
   screenshotsDir: './screenshots',
   runs: 80,
